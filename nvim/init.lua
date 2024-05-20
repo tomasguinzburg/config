7620e2b73e881f1f0e2a3c19ed0e0767d4837b7f
Tomás Guinzburg <tomasguinzburg@gmail.com>
2024-05-20 09:19:15 +0200

    templ

--- nvim/init.lua
+++ nvim/init.lua
@@ -733,7 +733,7 @@ cmp.setup {
   },
 }
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "120"
 -- Prepend mise shims to PATH
 vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
 
